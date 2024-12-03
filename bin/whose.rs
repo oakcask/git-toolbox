6a080d97db0422ec1d57b1ab7d7b2e2931f424e8
Kuya KOHARA <kuya.kohara@gmail.com>
2024-12-04 01:20:33 +0900

    refactor(git-whose): main logic into Application type

--- bin/whose.rs
+++ bin/whose.rs
@@ -1,8 +1,6 @@
 use clap::Parser;
-use git2::{Pathspec, PathspecFlags, Repository};
-use git_toolbox::{github::codeowners::CodeOwners, pathname};
-use log::error;
-use std::{error::Error, ffi::OsStr, os::unix::ffi::OsStrExt as _, process::exit};
+use git2::Repository;
+use git_toolbox::app::whose::{Application, ApplicationBuilder};
 
 #[derive(Parser)]
 #[command(
@@ -13,64 +11,18 @@ struct Cli {
     pathspecs: Vec<String>,
 }
 
-struct Command {
-    repo: Repository,
-    codeowners: CodeOwners,
-    pathspecs: Vec<String>,
-}
-
-impl Command {
-    fn run(&self) -> Result<(), Box<dyn Error>> {
-        let index = self.repo.index()?;
-        let pathspec = Pathspec::new(self.pathspecs.iter())?;
-        let matches = pathspec.match_index(&index, PathspecFlags::default())?;
-
-        for entry in matches.entries() {
-            let path = OsStr::from_bytes(entry);
-            if let Some(path) = OsStr::from_bytes(entry).to_str() {
-                match self.codeowners.find_owners(path) {
-                    Some(owners) => {
-                        println!("{}: {}", path, owners.join(", "));
-                    }
-                    None => {
-                        println!("{}:", path);
-                    }
-                }
-            } else {
-                log::error!("cannot convet {:?} into utf-8 string.", path)
-            }
-        }
-
-        Ok(())
-    }
-}
-
 impl Cli {
-    fn into_command(self) -> Result<Command, Box<dyn Error>> {
+    fn into_app(self) -> Result<Application, Box<dyn std::error::Error>> {
         let repo = Repository::open_from_env()?;
-        let pathspecs = if repo.is_bare() {
-            self.pathspecs
-        } else {
-            pathname::normalize_paths(&repo, self.pathspecs)?
-        };
-
-        let codeowners = CodeOwners::try_from_repo(&repo)?;
-
-        Ok(Command {
-            repo,
-            codeowners,
-            pathspecs,
-        })
+        Ok(ApplicationBuilder::new(repo).with_pathspecs(self.pathspecs)?.build()?)
     }
 }
 
-fn main() -> ! {
-    env_logger::init();
-    match Cli::parse().into_command().and_then(|cmd| cmd.run()) {
-        Err(e) => {
-            error!("{}", e.to_string());
-            exit(1)
-        }
-        Ok(_) => exit(0),
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if let Err(e) = Cli::parse().into_app().and_then(|cmd| cmd.run().map_err(|e| e.into())) {
+        eprintln!("{}", e);
+        Err(e)
+    } else {
+        Ok(())
     }
 }
---
+++ src/app/whose.rs
@@ -0,0 +1,87 @@
+use std::{ffi::OsStr, os::unix::ffi::OsStrExt as _};
+
+use git2::{Pathspec, PathspecFlags, Repository};
+use log::info;
+
+use crate::{github::codeowners::{CodeOwners, CodeOwnersError}, pathname};
+
+pub struct Application {
+    pub repo: Repository,
+    pub codeowners: CodeOwners,
+    pub pathspecs: Vec<String>,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum ApplicationError {
+    #[error("{0}")]
+    Git(#[from] git2::Error),
+    #[error("{0}")]
+    PathError(#[from] pathname::NormalizePathError),
+    #[error("{0}")]
+    CodeOwnersError(#[from] CodeOwnersError)
+}
+
+impl Application {
+    pub fn run(&self) -> Result<(), ApplicationError> {
+        env_logger::init();
+
+        let index = self.repo.index()?;
+        let pathspec = Pathspec::new(self.pathspecs.iter())?;
+        let matches = pathspec.match_index(&index, PathspecFlags::default())?;
+
+        for entry in matches.entries() {
+            let path = OsStr::from_bytes(entry);
+            if let Some(path) = OsStr::from_bytes(entry).to_str() {
+                match self.codeowners.find_owners(path) {
+                    Some(owners) => {
+                        println!("{}: {}", path, owners.join(", "));
+                    }
+                    None => {
+                        println!("{}:", path);
+                    }
+                }
+            } else {
+                log::error!("cannot convet {:?} into utf-8 string.", path)
+            }
+        }
+
+        Ok(())
+    }
+}
+
+pub struct ApplicationBuilder {
+    repo: Repository,
+    pathspecs: Vec<String>,
+}
+
+impl ApplicationBuilder {
+    pub fn new(repo: Repository) -> Self {
+        Self {
+            repo,
+            pathspecs: Default::default(),
+        }
+    }
+
+    pub fn with_pathspecs(self, pathspecs: Vec<String>) -> Result<Self, ApplicationError> {
+        let pathspecs = if self.repo.is_bare() {
+            info!("this is bare repository");
+            self.pathspecs
+        } else {
+            pathname::normalize_paths(&self.repo, pathspecs)?
+        };
+
+        Ok(Self {
+            pathspecs,
+            ..self
+        })
+    }
+
+    pub fn build(self) -> Result<Application, ApplicationError> {
+        let codeowners = CodeOwners::try_from_repo(&self.repo)?;
+        Ok(Application {
+            repo: self.repo,
+            codeowners,
+            pathspecs: self.pathspecs,
+        })
+    }
+}
