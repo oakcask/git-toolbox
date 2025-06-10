f43c3509a3aed16558e8aa1758a20222967ddf5c
Kuya KOHARA <kuya.kohara@gmail.com>
2025-06-10 13:57:54 +0900

    feat(git-whose): support `/CODEOWNERS` and `docs/CODEOWNERS` as location
    closes #239

--- tests/codeowners.rs
+++ tests/codeowners.rs
@@ -1,18 +1,25 @@
 mod support;
 
 use git_toolbox::github::codeowners::{CodeOwners, CodeOwnersError};
+use rstest::rstest;
 use support::{git_add, git_init, mkdir_p, test_logger, write};
 use tempfile::TempDir;
 
-#[test]
-fn codeowner_try_from_repo_fails_when_codeowners_file_is_not_indexed() {
+#[rstest]
+#[case(".github/CODEOWNERS")]
+#[case("CODEOWNERS")]
+#[case("docs/CODEOWNERS")]
+fn codeowner_try_from_repo_fails_when_codeowners_file_is_not_indexed_(
+    #[case] path: &str
+) {
     let tmpdir = TempDir::new().unwrap();
     let root = tmpdir.path();
 
     let repo = git_init(root);
-    mkdir_p(root.join(".github"));
+    let co_path = root.join(path);
+    mkdir_p(co_path.parent().unwrap());
     write(
-        root.join(".github/CODEOWNERS"),
+        co_path,
         "\
 # comment line
 *.js frontend-developer # comment
@@ -28,16 +35,22 @@ baz/ baz-owner
     ));
 }
 
-#[test]
-fn codeowner_try_from_repo_does_not_generate_warnings_for_lines_without_pattern() {
+#[rstest]
+#[case(".github/CODEOWNERS")]
+#[case("CODEOWNERS")]
+#[case("docs/CODEOWNERS")]
+fn codeowner_try_from_repo_does_not_generate_warnings_for_lines_without_pattern(
+    #[case] path: &str,
+) {
     let logger = test_logger();
     let tmpdir = TempDir::new().unwrap();
     let root = tmpdir.path();
 
     let repo = git_init(root);
-    mkdir_p(root.join(".github"));
+    let co_path = root.join(path);
+    mkdir_p(co_path.parent().unwrap());
     write(
-        root.join(".github/CODEOWNERS"),
+        co_path,
         "\
 # comment line
 *.js frontend-developer # comment
@@ -47,7 +60,7 @@ baz/ baz-owner
         .as_bytes(),
     );
 
-    git_add(&repo, ".github/CODEOWNERS");
+    git_add(&repo, path);
 
     let co = CodeOwners::<()>::try_from_repo(&repo).unwrap();
     assert_eq!(logger.take(), vec![]);
@@ -55,5 +68,44 @@ baz/ baz-owner
     assert_eq!(
         co.find_owners("foo/bar.js"),
         Some(&vec![String::from("frontend-developer")])
+    );
+}
+
+#[rstest]
+#[case(".github/CODEOWNERS", "CODEOWNERS")]
+#[case("CODEOWNERS", "docs/CODEOWNERS")]
+fn codeowner_try_from_repo_find_codeowners_file_in_priority(
+    #[case] prior: &str,
+    #[case] after: &str,
+) {
+    let tmpdir = TempDir::new().unwrap();
+    let root = tmpdir.path();
+
+    let repo = git_init(root);
+    let co1_path = root.join(prior);
+    let co2_path = root.join(after);
+    mkdir_p(co1_path.parent().unwrap());
+    mkdir_p(co2_path.parent().unwrap());
+    write(
+        co1_path,
+        "\
+*.js owner-1
+"
+        .as_bytes(),
+    );
+    write(
+        co2_path,
+        "\
+*.js owner-2
+"
+        .as_bytes(),
+    );
+    git_add(&repo, prior);
+    git_add(&repo, after);
+    
+    let co = CodeOwners::<()>::try_from_repo(&repo).unwrap();
+    assert_eq!(
+        co.find_owners("a.js"),
+        Some(&vec![String::from("owner-1")])
     );
 }
